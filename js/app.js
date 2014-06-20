efd8b7c0acd81e99c4102dfbe30014f07aabf91a
Jennifer Turner <jenniferturner202@gmail.com>
2014-06-20 10:15:03 -0400

    FizzBuzz File Upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function fizzBuzz(num){
+	if ((num % 3) == 0 && (num % 5) !== 0) {
+		return ("Fizz");
+	} else if 
+	((num % 5) == 0 && (num % 3) !== 0 {
+		return("Buzz"); 
+	} else if 
+	((num % 3) == 0 && (num % 5) === 0) {
+		return("FizzBuzz"); 
+		} else {
+		return num;
+				}
+	}
+return fizzBuzz(100);
+	$(function(){
+		$(document).appendTo('body');
+
+	});
+	
