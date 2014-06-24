2d311b56512973b306159d0fe3b10ec6b46fd0b3
Jennifer Turner <jenniferturner202@gmail.com>
2014-06-24 14:56:36 -0400

    Adjusted functions

--- js/app.js
+++ js/app.js
@@ -1,30 +1,58 @@
-//$(document).ready(function(){
+$(document).ready(function() {
 
-var num = 100;
-$(function fizzBuzz(num) { 
-  //var fizzCount = 0;
-for (var i = 1; i <= num; i += num) {
-  if ($(num % 3) === 0 && (num % 5) !== 0) {
+  function addEntry() {
+  var entry = $('#number-entry').val();
+  $('#number-entry').val('');
+event.preventDefault();
+}
+
+// Integrate user input into the loop
+//
+for (i = 1; i <= 100; i+=1) {
+  if ( i % 3 === 0) {
     $('#output').append('<p>Fizz</p>');
-//return ("Fizz");
-  } else if 
-  ($(num % 5) === 0 && (num % 3) !== 0) {
+  } else if (i % 5 === 0) {
     $('#output').append('<p>Buzz</p>');
-  //  return("Buzz");
+  } else if ( i % 3 && i % 5 === 0) {
+    $('#output').append('<p>FizzBuzz</p>');
   } else {
-  ($(num % 3) === 0 && (num % 5) === 0){
-    $('#output').append('<p>FizzBuzz<p>');
+    $('#output').append('<p></p>'+i+'<p></p>');
+  }
+}
+});
+
+$(function(){
+$('#number-entry').on('submit', addEntry);
+document.forms["fizzBust"].reset();
+});
+
+
+
+//var num = 100;
+//$(function fizzBuzz(num) { 
+  //var fizzCount = 0;
+//for (var i = 1; i <= num; i += num) {
+  //if ($(num % 3) === 0 && (num % 5) !== 0) {
+    //$('#output').append('<p>Fizz</p>');
+//return ("Fizz");
+  // } else if 
+  // ($(num % 5) === 0 && (num % 3) !== 0) {
+    // $('#output').append('<p>Buzz</p>');
+  //  return("Buzz");
+  // } else {
+  // ($(num % 3) === 0 && (num % 5) === 0){
+    // $('#output').append('<p>FizzBuzz<p>');
     //return("FizzBuzz");
-}
-}
-}
+// }
+// }
+// }
 
 //function addOutput(){
   //$('')
 //}
 
-    $(function(){
-      $('form').on('submit',fizzBuzz);
-    });
-  });
-});
+   // $(function(){
+     // $('form').on('submit',fizzBuzz);
+   // });
+  // });
+// });
