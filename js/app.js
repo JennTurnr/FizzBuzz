28b67fad3fe1f3ed14121d9e498c8299d26a87cd
Jennifer Turner <jenniferturner202@gmail.com>
2014-06-25 09:53:51 -0400

    Fixed undefined error line 43

--- js/app.js
+++ js/app.js
@@ -1,20 +1,13 @@
 $(document).ready(function () {
 
     function addEntry(event) {
-            var entry = $('#number-entry').val();
-            $('#output').append('<div>'+entry+'</div>');
+            var entry = $('#number-entry').val(); 
+            fizzBuzz(entry);
+           $('#output').append('<body>'+entry+'</body>');
             $('#number-entry').val('');
-            event.preventDefault();
-        }
-        //jsbeautifier for addEntry http://jsbeautifier.org/
-        // lookup jQuery event variable
-
-    //put loop inside of a function and take a variable and take in a number (100)
-
-    // Integrate user input into the loop
-    //
-
-    var limit = 100;
+            event.preventDefault();     
+}
+      
    function fizzBuzz(limit) {
         for (i = 1; i <= limit; i += 1) {
             if (i % 3 === 0 && i % 5 === 0) {
@@ -28,47 +21,12 @@ $(document).ready(function () {
             }
         }
     }
-});
 
-// if (i % 3 == 0 && i % 5 === 0) {
-//$('#output').append('<p>FizzBuzz</p>')
-
-// 
-
-
-
+// Run on submit
 $(function () {
-    $('form').on(fizzBuzz,'submit', addEntry);
+    $('form').on('submit', addEntry);
         document.forms["fizzBust"].reset();
+    });
 });
 
-
-//$('#number-entry').closest('form')
-//var limit = 100;
-//$(function fizzBuzz(num) { 
-//var fizzCount = 0;
-//for (var i = limit; i <= limit; i += 1) {
-//if ($(num % 3) === 0 && (num % 5) !== 0) {
-//$('#output').append('<p>Fizz</p>');
-//return ("Fizz");
-// } else if 
-// ($(i % 5) === 0 && (i % 3) !== 0) {
-// $('#output').append('<p>Buzz</p>');
-//  return("Buzz");
-// } else {
-// ($(i % 3) === 0 && (i % 5) === 0){
-// $('#output').append('<p>FizzBuzz<p>');
-//return("FizzBuzz");
-// } 
-// }
-// }
-
-//function addOutput(){
-//$('')
-//}
-
-// $(function(){
-// $('form').on('submit',fizzBuzz);
-// });
-// });
-// });
+//June 25th - RICK
