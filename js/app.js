55b3dd4ca491e489fe068443d6853ae7e19a3778
Jennifer Turner <jenniferturner202@gmail.com>
2014-06-24 23:04:37 -0400

    Added variable & adjusted conditional statement

--- js/app.js
+++ js/app.js
@@ -1,58 +1,74 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  function addEntry() {
-  var entry = $('#number-entry').val();
-  $('#number-entry').val('');
-event.preventDefault();
-}
+    function addEntry(event) {
+            var entry = $('#number-entry').val();
+            $('#output').append('<div>'+entry+'</div>');
+            $('#number-entry').val('');
+            event.preventDefault();
+        }
+        //jsbeautifier for addEntry http://jsbeautifier.org/
+        // lookup jQuery event variable
 
-// Integrate user input into the loop
-//
-for (i = 1; i <= 100; i+=1) {
-  if ( i % 3 === 0) {
-    $('#output').append('<p>Fizz</p>');
-  } else if (i % 5 === 0) {
-    $('#output').append('<p>Buzz</p>');
-  } else if ( i % 3 && i % 5 === 0) {
-    $('#output').append('<p>FizzBuzz</p>');
-  } else {
-    $('#output').append('<p></p>'+i+'<p></p>');
-  }
-}
+    //put loop inside of a function and take a variable and take in a number (100)
+
+    // Integrate user input into the loop
+    //
+
+    var limit = 100;
+   function fizzBuzz(limit) {
+        for (i = 1; i <= limit; i += 1) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                $('#output').append('<p>FizzBuzz</p>');
+            } else if (i % 3 === 0) {
+                $('#output').append('<p>Fizz</p>');
+            } else if (i % 5 === 0) {
+                $('#output').append('<p>Buzz</p>');
+            } else {
+                $('#output').append('<p>' + i + '</p>'); //'<p>'
+            }
+        }
+    }
 });
 
-$(function(){
-$('#number-entry').on('submit', addEntry);
-document.forms["fizzBust"].reset();
+// if (i % 3 == 0 && i % 5 === 0) {
+//$('#output').append('<p>FizzBuzz</p>')
+
+// 
+
+
+
+$(function () {
+    $('form').on(fizzBuzz,'submit', addEntry);
+        document.forms["fizzBust"].reset();
 });
 
 
-
-//var num = 100;
+//$('#number-entry').closest('form')
+//var limit = 100;
 //$(function fizzBuzz(num) { 
-  //var fizzCount = 0;
-//for (var i = 1; i <= num; i += num) {
-  //if ($(num % 3) === 0 && (num % 5) !== 0) {
-    //$('#output').append('<p>Fizz</p>');
+//var fizzCount = 0;
+//for (var i = limit; i <= limit; i += 1) {
+//if ($(num % 3) === 0 && (num % 5) !== 0) {
+//$('#output').append('<p>Fizz</p>');
 //return ("Fizz");
-  // } else if 
-  // ($(num % 5) === 0 && (num % 3) !== 0) {
-    // $('#output').append('<p>Buzz</p>');
-  //  return("Buzz");
-  // } else {
-  // ($(num % 3) === 0 && (num % 5) === 0){
-    // $('#output').append('<p>FizzBuzz<p>');
-    //return("FizzBuzz");
-// }
+// } else if 
+// ($(i % 5) === 0 && (i % 3) !== 0) {
+// $('#output').append('<p>Buzz</p>');
+//  return("Buzz");
+// } else {
+// ($(i % 3) === 0 && (i % 5) === 0){
+// $('#output').append('<p>FizzBuzz<p>');
+//return("FizzBuzz");
+// } 
 // }
 // }
 
 //function addOutput(){
-  //$('')
+//$('')
 //}
 
-   // $(function(){
-     // $('form').on('submit',fizzBuzz);
-   // });
-  // });
+// $(function(){
+// $('form').on('submit',fizzBuzz);
+// });
+// });
 // });
